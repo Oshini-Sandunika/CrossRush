dd41faab1780412454314092479fbf6f003e8594
Oshini-Sandunika <oshinisandunika416@gmail.com>
2025-06-28 11:12:46 +0530

    Player animation implemented

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,22 +4,55 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
 
     private void Awake()
     {
+        //Grab reference for rigidbody and animator from object 
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
         body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocity.y);
 
-        if (Input.GetKey(KeyCode.Space))
+        //Flip player when moving left and right
+        if(horizontalInput > 0.01f)
         {
-            body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
+            transform.localScale = Vector3.one;
         }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1,1,1);
+        }
+
+        if (Input.GetKey(KeyCode.Space) && grounded)
+        {
+            Jump();
+        }
+
+        //Set animator parameters
+        anim.SetBool("Run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
     
-    
+    }
+
+    private void Jump()
+    {
+        body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
+        grounded = false;
+        anim.SetTrigger("jump");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
     }
 
 
